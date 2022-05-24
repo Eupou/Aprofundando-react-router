9f3e466fd083f7d0b9360440d778f1570cdcda0d
Eupou <wesleywesleygomesgomes@gmail.com>
2022-05-24 15:19:54 -0300

    feat: made the course till search params

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from "./logo.svg";
+import { Outlet, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <h1>Bookkeeper!</h1>
+
+      <nav>
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+      <Outlet />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+import App from "./App";
+
+import Expenses from "./pages/Expenses";
+import Invoices from "./pages/Invoices";
+import Invoice from "./pages/Invoice";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="expenses" element={<Expenses />} />
+          <Route path="invoices" element={<Invoices />}>
+            <Route
+              index
+              element={<p style={{ padding: "1rem" }}>Select an invoice</p>}
+            />
+            <Route path=":invoiceId" element={<Invoice />} />
+          </Route>
+          <Route path="*" element={<main>There's nothing here!</main>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
